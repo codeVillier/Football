a481cd90d44219950f1ad3d9bf90da202a1550b9
Oscar <odevill@icloud.com>
2025-11-17 16:05:02 +0200

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Oscar De Villiers on 2025/11/17.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
